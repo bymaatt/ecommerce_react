002425e66ea819fdae271a2fde41e9cff1f6ecf1
Matias Maciel <mattias.maciel@gmail.com>
2022-01-02 21:46:33 -0300

    arreglo de etiqueta a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,23 +7,23 @@ function NavBar() {
         <div>
            <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Impo Coleccionables</a>
+                    <p class="navbar-brand" href="#">Impo Coleccionables</p>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Inicio</a>
+                            <p class="nav-link">Inicio</p> 
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Colección </a>
+                            <p >Colección</p> 
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
+                            <p class="nav-link">Contacto</p>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link">Tienda</a>
+                            <p class="nav-link">Tienda</p>
                             </li>
                         </ul>
                     </div>
