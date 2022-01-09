d138d5bcf0c2d71502a410002232e02a29d48bce
Matias Maciel <mattias.maciel@gmail.com>
2022-01-08 21:20:13 -0300

    add components

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ItemListContainer = () => {
+    return (
+        <View>
+            <Text><h1> Mi app con react</h1></Text>
+        </View>
+    )
+}
+
+export default ItemListContainer
