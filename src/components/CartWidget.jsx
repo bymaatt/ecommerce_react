24553192e8f24c69d7b635c0e89db8aec77c953c
Matias Maciel <mattias.maciel@gmail.com>
2022-01-09 17:22:31 -0300

    cambio ruta img

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import carrito from './assets/img/carrito.png'
 
 function CartWidget() {
     return (
         <div>
-            <img id='carrito' src="./carrito.png" alt="carrito" />
+            <img id='carrito' src={carrito} alt="carrito" />
         </div>
     )
 }
 
 export default CartWidget
+ 
