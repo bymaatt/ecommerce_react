055dc9d725144c19b2439ecfb78e27f6809a2cad
Matias Maciel <mattias.maciel@gmail.com>
2022-01-09 21:24:58 -0300

    rename class-className

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -7,25 +7,25 @@ import CartWidget from '../CartWidget/CartWidget';
 function NavBar() {
     return (
         <div>
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a href="/#" class="navbar-brand">Impo Coleccionables</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+           <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a href="/#" className="navbar-brand">Impo Coleccionables</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                            <a href= "/#" class="nav-link">Inicio</a> 
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                            <a href= "/#" className="nav-link">Inicio</a> 
                             </li>
-                            <li class="nav-item">
-                            <a href="/#" class="nav-link">Colección</a> 
+                            <li className="nav-item">
+                            <a href="/#" className="nav-link">Colección</a> 
                             </li>
-                            <li class="nav-item">
-                            <a href="/#" class="nav-link">Contacto</a>
+                            <li className="nav-item">
+                            <a href="/#" className="nav-link">Contacto</a>
                             </li>
-                            <li class="nav-item">
-                            <a href="/#" class="nav-link">Tienda</a>
+                            <li className="nav-item">
+                            <a href="/#" className="nav-link">Tienda</a>
                             </li>
                         </ul>
                     </div>
