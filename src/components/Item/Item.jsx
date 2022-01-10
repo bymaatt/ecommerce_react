aec349a15fe51c542b8fed188c8a454405d97aba
Matias Maciel <mattias.maciel@gmail.com>
2022-01-10 18:37:12 -0300

    css de los productos + arreglo de img

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './Item.css'
 
+
 function Item({productos}) {
     return (
-        <div>
-            <h2>{productos.nombre}</h2>
-            <h3>
-                Precio: $ {productos.precio}
-            </h3>
-            <h3>
-                stock: {productos.stock} unidades
-            </h3>
-            <p>
-                {productos.descripcion}
-            </p>
-            <img id='imgProd' src= {productos.imagen} alt= {productos.nombre} />
+        <div className='arreglo_mio'>
+            <div className='arreglo_img'>
+                <img id="img_arreglo" src={productos.imagen} className="card-img-top" alt={productos.nombre}/>
+            </div>
+            <div className="arreglo_description">
+                <h2 className="card-title">{productos.nombre}</h2>
+                <h6 className="card-title">stock: {productos.stock} unidades</h6>
+                <p className="card-text">Descripción: {productos.descripcion}</p>
+                <a href="/#" className="btn btn-primary">Comprar</a>
+            </div>
+            
         </div>
     )
 }
