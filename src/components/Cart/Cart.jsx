73fdb2da52a0c4df4b2844150cabde0f3ede3a9c
Matias Maciel <mattias.maciel@gmail.com>
2022-01-26 00:05:53 -0300

    emprolijando cart vacío

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import './Cart.css'
 import { useCartContext } from '../Context/CartContext'
+import { Link } from 'react-router-dom'
 
 
 const Cart=() => {
     const {cartList, deleteItem, vaciarCarrito} =useCartContext()
     return (
-        <div className='contenedor_master'>
-            <div className='contenedor_detalle'>
-                {cartList.map(productos =><ul className=" item card-title"><li key={productos.id}>{productos.nombre} - cant: {productos.counter} <button className='btns1' onClick={() => deleteItem(productos.id)}>x</button></li></ul>)}
-                
-            </div>
-            <div>
-                
-            </div>
-            <div className='butt_master'>
-                <button className='btn-dark butt' onClick={vaciarCarrito}>Vaciar Carrito</button>
-            </div>
-        </div>
+        <>
+            {cartList.length ===0 ?(
+                <div>
+                    <h1>No has agregado nada en el carrito aún</h1>
+                    <Link to='/'>
+                        <button className='btns1 btn-dark'> Ir a la tienda</button>
+                    </Link>
+                </div>
+                ) : (
+                <>
+                    <div className='contenedor_master'>
+                        <div className='contenedor_detalle'>
+                            {cartList.map(productos =><ul className=" item card-title"><li key={productos.id}>{productos.nombre} - cant: {productos.counter} <button className='btns1' onClick={() => deleteItem(productos.id)}>x</button></li></ul>)}
+                        </div>
+                        <div className='butt_master'>
+                            <button className='btn-dark butt' onClick={vaciarCarrito}>Vaciar Carrito</button>
+                        </div>
+                    </div>
+                    
+                </>
+                )}
+        </>
     )
 }
 
