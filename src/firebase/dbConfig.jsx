a8fd8ff7eb0c1f02f6376211ea46eb7688e8fa79
userTrabajo <matias.maciel@biosintex.com.ar>
2022-01-28 09:54:34 -0300

    arreglo db config

--- src/firebase/dbConfig.jsx
+++ src/firebase/dbConfig.jsx
@@ -1,7 +1,9 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDzQ9kU2iulEevZbMrAxyMiTpaYh3eDqwc",
   authDomain: "ecommerce-react-63edc.firebaseapp.com",
@@ -11,10 +13,9 @@ const firebaseConfig = {
   appId: "1:754766522011:web:869a8659b74977a3f7883e"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const getFireStoreApp =()=>{
+export const getFirestoreApp =()=>{
     return app
 }
-    
