72f67f3c59537eda1d7e866c1d38ef9786508b31
userTrabajo <matias.maciel@biosintex.com.ar>
2022-02-02 11:31:45 -0300

    el arreglo definitivo- cambio main x index

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 import App from "../App";
-import { getFirestoreApp } from '../firebase/dbConfig';
 
-
-getFirestoreApp()
 
 ReactDom.render(
     
