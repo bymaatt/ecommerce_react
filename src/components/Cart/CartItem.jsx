9dbbfdb6fe134b4b3f8fcdb2fdd560b956d46fcc
Matias Maciel <mattias.maciel@gmail.com>
2022-02-06 17:28:49 -0300

    arreglo  cart li

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -84,7 +84,7 @@ const CartItem = () => {
   return (
     <div className='contenedor_master'>
         <div className='contenedor_detalle'>
-            {cartList.map(productos =><ul className=" item card-title"><li key={productos.id}>{productos.nombre} - cant: {productos.counter} <li className='boton'><button className='btns1' onClick={() => deleteItem(productos.id)}>x</button></li></li></ul>)}
+            {cartList.map(productos =><ul className=" item card-title"><li key={productos.id}>{productos.nombre} - cant: {productos.counter} <button className='btns1' onClick={() => deleteItem(productos.id)}>x</button></li></ul>)}
         </div>
         <div className='totales'>
             <div className='precio_total'>
